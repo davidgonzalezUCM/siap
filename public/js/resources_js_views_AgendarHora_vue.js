d58bbdb5fce7d4a2c00669c373e309e4fdb31279
davidgonzalezUCM <pinwiino034@gmail.com>
2022-10-06 23:42:24 -0400

    cambios a formulario de agendar hora

--- public/js/resources_js_views_AgendarHora_vue.js
+++ public/js/resources_js_views_AgendarHora_vue.js
@@ -20,8 +20,8 @@ __webpack_require__.r(__webpack_exports__);
   },
   data: function data() {
     return {
-      fechaN: null,
-      date: new Date(),
+      fechaN: '',
+      date: '',
       hours: Array.from({
         length: 9
       }).map(function (_, i) {
@@ -51,6 +51,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "render": () => (/* binding */ render)
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
+/* harmony import */ var _resources_images_atencion_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../resources/images/atencion.jpg */ "./resources/images/atencion.jpg");
+
 
 var _hoisted_1 = {
   "class": "our-services"
@@ -58,74 +60,126 @@ var _hoisted_1 = {
 var _hoisted_2 = {
   "class": "container"
 };
-
-var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"row mb-3\"><label class=\"form-label\">Nombre Completo</label><div class=\"col-sm-3\"><input type=\"text\" class=\"form-control\" placeholder=\"Nombres\" id=\"inputEmail3\"></div><div class=\"col-sm-2\"><input type=\"text\" class=\"form-control\" placeholder=\"Apellido Paterno\" id=\"inputEmail3\"></div><div class=\"col-sm-2\"><input type=\"text\" class=\"form-control\" placeholder=\"Apellio Materno\" id=\"inputEmail3\"></div></div><div class=\"row mb-3\"><label class=\"form-label\">Rut:</label><div class=\"col-sm-3\"><input class=\"form-control\" type=\"text\" name=\"Rut\" id=\"\"></div></div>", 2);
-
-var _hoisted_5 = {
-  "class": "mb-3"
+var _hoisted_3 = {
+  "class": "row"
+};
+var _hoisted_4 = {
+  "class": "col-md-6"
 };
 
-var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"row mb-3\"><label class=\"form-label\">Nombre Completo</label><div class=\"col\"><input type=\"text\" class=\"form-control\" placeholder=\"Nombres\" id=\"inputEmail3\"></div><div class=\"col\"><input type=\"text\" class=\"form-control\" placeholder=\"Apellido Paterno\" id=\"inputEmail3\"></div><div class=\"col\"><input type=\"text\" class=\"form-control\" placeholder=\"Apellio Materno\" id=\"inputEmail3\"></div></div><div class=\"row mb-3\"><div class=\"col\"><label class=\"form-label\">Rut:</label><input class=\"form-control\" type=\"text\" name=\"Rut\" id=\"\"></div><div class=\"col\"><label class=\"form-label\">telefono:</label><input class=\"form-control\" type=\"text\" name=\"telefono\" id=\"\"></div></div>", 2);
+
+var _hoisted_7 = {
+  "class": "row mb-3"
+};
+
+var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "col"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "class": "form-label"
+}, "Correo Electronico:"), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  "class": "form-control",
+  type: "email",
+  name: "correo",
+  id: ""
+})], -1
+/* HOISTED */
+);
+
+var _hoisted_9 = {
+  id: "calendario",
+  "class": "col"
+};
+
+var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
   "class": "form-label"
 }, "Fecha de nacimento:", -1
 /* HOISTED */
 );
 
-var _hoisted_7 = {
-  id: "calendario"
+var _hoisted_11 = {
+  "class": "row mb-3"
 };
 
-var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"mb-3\"><label>Correo Electronico:</label><div class=\"col-sm-3\"><input class=\"form-control\" type=\"email\" name=\"correo\" id=\"\"></div></div><div class=\"mb-3\"><label class=\"form-label\">telefono:</label><div class=\"col-sm-3\"><input class=\"form-control\" type=\"text\" name=\"telefono\" id=\"\"></div></div><div class=\"mb-3\"><label class=\"form-label\">ciudad:</label><div class=\"col-sm-3\"><input class=\"form-control\" type=\"text\" name=\"ciudad\" id=\"\"></div></div><div class=\"mb-3\"><label class=\"form-label\">Motivo de Consulta:</label><div class=\"col-sm-3\"><textarea class=\"form-control\" name=\"motivo\" id=\"\" rows=\"3\"></textarea></div></div>", 4);
+var _hoisted_12 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "col"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "class": "form-label"
+}, "ciudad:"), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  "class": "form-control",
+  type: "text",
+  name: "ciudad",
+  id: ""
+})], -1
+/* HOISTED */
+);
 
-var _hoisted_12 = {
-  "class": "mb-3"
+var _hoisted_13 = {
+  id: "calendario",
+  "class": "col"
 };
 
-var _hoisted_13 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+var _hoisted_14 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
   "class": "form-label"
 }, "Agendar Hora:", -1
 /* HOISTED */
 );
 
-var _hoisted_14 = {
-  id: "calendario"
-};
+var _hoisted_15 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"row mb-3\"><label class=\"form-label\">Motivo de Consulta:</label><div class=\"col\"><textarea class=\"form-control\" style=\"resize:none;\" name=\"motivo\" id=\"\" rows=\"3\"></textarea></div></div><input type=\"submit\" value=\"Agendar\" class=\"modal-btn\">", 2);
 
-var _hoisted_15 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
-  type: "submit",
-  value: "Agendar",
-  "class": "modal-btn"
-}, null, -1
+var _hoisted_17 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "col-md-4"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
+  id: "imgatencion",
+  src: _resources_images_atencion_jpg__WEBPACK_IMPORTED_MODULE_1__["default"],
+  alt: "Logo Psicóloga"
+})], -1
 /* HOISTED */
 );
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_datepicker = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("datepicker");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("section", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("form", null, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [_hoisted_6, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_datepicker, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("section", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("form", _hoisted_4, [_hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [_hoisted_8, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_9, [_hoisted_10, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_datepicker, {
     value: $data.fechaN,
     "onUpdate:value": _cache[0] || (_cache[0] = function ($event) {
       return $data.fechaN = $event;
     }),
+    editable: false,
+    clearable: false,
     type: "date",
     format: "DD-MM-YYYY"
   }, null, 8
   /* PROPS */
-  , ["value"])])]), _hoisted_8, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [_hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_14, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_datepicker, {
+  , ["value"])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_11, [_hoisted_12, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_13, [_hoisted_14, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_datepicker, {
     value: $data.date,
     "onUpdate:value": _cache[1] || (_cache[1] = function ($event) {
       return $data.date = $event;
     }),
     "minute-step": 10,
     "hour-options": $data.hours,
-    "default-value": new Date(),
     "disabled-date": $options.disabledBeforeTodayAndAfterAWeek,
+    editable: false,
     type: "datetime",
     format: "DD-MM-YYYY HH:mm"
   }, null, 8
   /* PROPS */
-  , ["value", "hour-options", "default-value", "disabled-date"])])]), _hoisted_15])])]);
+  , ["value", "hour-options", "disabled-date"])])]), _hoisted_15]), _hoisted_17])])]);
 }
+
+/***/ }),
+
+/***/ "./resources/images/atencion.jpg":
+/*!***************************************!*\
+  !*** ./resources/images/atencion.jpg ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/atencion.jpg?f1d65d157034745049d1d0e6fd183500");
 
 /***/ }),
 
--- resources/js/views/AgendarHora.vue
+++ resources/js/views/AgendarHora.vue
@@ -1,70 +1,73 @@
 <template>
   <section class="our-services">
     <div class="container">
-      <form>
+    <div class="row">
+      <form class="col-md-6">
 
         <div class="row mb-3">
           <label class="form-label">Nombre Completo</label>
-          <div class="col-sm-3">
+          <div class="col">
             <input type="text" class="form-control" placeholder="Nombres" id="inputEmail3">
           </div>
-          <div class="col-sm-2">
+          <div class="col">
             <input type="text" class="form-control" placeholder="Apellido Paterno" id="inputEmail3">
           </div>
-          <div class="col-sm-2">
+          <div class="col">
             <input type="text" class="form-control" placeholder="Apellio Materno" id="inputEmail3">
           </div>
         </div>
 
         <div class="row mb-3">
-          <label class="form-label">Rut:</label>
-          <div class="col-sm-3">
+          <div class="col">
+            <label class="form-label">Rut:</label>
             <input class="form-control" type="text" name="Rut" id="" />
           </div>
-        </div>
 
-        <div class="mb-3">
-          <label class="form-label">Fecha de nacimento:</label>
-          <div id="calendario">
-            <datepicker v-model:value="fechaN" type="date" format="DD-MM-YYYY"></datepicker>
-          </div>
-        </div>
-
-        <div class="mb-3">
-          <label>Correo Electronico:</label>
-          <div class="col-sm-3">
-            <input class="form-control" type="email" name="correo" id="" />
-          </div>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">telefono:</label>
-          <div class="col-sm-3">
+          <div class="col">
+            <label class="form-label">telefono:</label>
             <input class="form-control" type="text" name="telefono" id="" />
           </div>
         </div>
-        <div class="mb-3">
-          <label class="form-label">ciudad:</label>
-          <div class="col-sm-3">
+
+
+        <div class="row mb-3">
+          <div class="col">
+            <label class="form-label">Correo Electronico:</label>
+            <input class="form-control" type="email" name="correo" id="" />
+          </div>
+          <div id="calendario" class="col">
+            <label class="form-label">Fecha de nacimento:</label>
+            <datepicker v-model:value="fechaN" :editable="false" :clearable="false" type="date" format="DD-MM-YYYY"></datepicker>
+          </div>
+
+        </div>
+
+        <div class="row mb-3">
+          <div class="col">
+            <label class="form-label">ciudad:</label>
             <input class="form-control" type="text" name="ciudad" id="" />
           </div>
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Motivo de Consulta:</label>
-          <div class="col-sm-3">
-            <textarea class="form-control" name="motivo" id="" rows="3"></textarea>
+
+          <div id="calendario" class="col">
+            <label class="form-label">Agendar Hora:</label>
+            <datepicker v-model:value="date" :minute-step="10" :hour-options="hours" 
+              :disabled-date="disabledBeforeTodayAndAfterAWeek" :editable="false"  type="datetime"  format="DD-MM-YYYY HH:mm"></datepicker>
           </div>
         </div>
-        <div class="mb-3">
-          <label class="form-label">Agendar Hora:</label>
-          <div id="calendario">
-            <datepicker v-model:value="date" :minute-step="10" :hour-options="hours" :default-value="new Date()"
-              :disabled-date="disabledBeforeTodayAndAfterAWeek" type="datetime" format="DD-MM-YYYY HH:mm"></datepicker>
+
+        <div class="row mb-3">
+          <label class="form-label">Motivo de Consulta:</label>
+          <div class="col">
+            <textarea class="form-control" style="resize: none;" name="motivo" id="" rows="3"></textarea>
           </div>
         </div>
 
         <input type="submit" value="Agendar" class="modal-btn" />
       </form>
-
+      <div class="col-md-4">
+        <img id="imgatencion" src="../../../resources/images/atencion.jpg" alt="Logo Psicóloga" />
+      </div>
+    </div>
     </div>
   </section>
 </template>
@@ -78,8 +81,8 @@ export default {
   },
   data() {
     return {
-      fechaN: null,
-      date: new Date(),
+      fechaN: '',
+      date: '',
       hours: Array.from({ length: 9 }).map((_, i) => i + 9),
     }
   },
